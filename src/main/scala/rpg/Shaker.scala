0f7405ef87b1515b70b3a64ae4ce0ac7ad1be302
Christian Krause <wookietreiber@users.berlios.de>
2011-07-01 07:30:28 +0200

    basic Shaker implementation

---
+++ src/main/scala/rpg/Shaker.scala
@@ -0,0 +1,55 @@
+/* **************************************************************************
+ *                                                                          *
+ *  Copyright (C)  2011  Christian Krause                                   *
+ *                                                                          *
+ *  Christian Krause <wookietreiber@users.berlios.de>                       *
+ *                                                                          *
+ ****************************************************************************
+ *                                                                          *
+ *  This file is part of 'rpg-tools'.                                       *
+ *                                                                          *
+ *  This project is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  any later version.                                                      *
+ *                                                                          *
+ *  This project is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this project. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+
+package rpg
+
+/** Dice-shaking actor. */
+class Shaker extends Actor {
+	/** Returns the random number generator. */
+	protected val random = new Random
+
+	// -----------------------------------------------------------------------
+	// message handling
+	// -----------------------------------------------------------------------
+
+	/** Handling done via `messageHandler orElse genericMessageHandler`. */
+	override final def receive = messageHandler orElse genericMessageHandler
+
+	/** Defines user specific message handling. */
+	protected def messageHandler: Receive = {
+		case null =>
+	}
+
+	private def genericMessageHandler: Receive = {
+		case die @ Die(sides) =>
+			val result = random.nextInt(sides) + 1
+			// EventHandler.info(self, "D" + sides + ": " + result)
+			self.channel ! result
+
+		case event =>
+			EventHandler.warning(self, "Don't know what to do with: " + event)
+	}
+}
