5660076b049d1d74df4fb16ebf9f8329d4c2838f
Christian Krause <kizkizzbangbang@googlemail.com>
2012-01-04 23:04:10 +0100

    build update

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import Dependencies._
 import BuildSettings._
 
 object BuildSettings {
-  val buildOrganization = "arpgt"
+  val buildOrganization = "com.github.wookietreiber"
   val buildVersion      = "0.1"
   val buildScalaVersion = "2.9.1"
   val akkaVersion       = "1.2"
@@ -44,7 +44,7 @@ object Dependencies {
   // test
   // -----------------------------------------------------------------------
 
-  val specs2 = "org.specs2" %% "specs2" % "1.6.1" % "test"
+  val specs2 = "org.specs2" %% "specs2" % "1.7.1" % "test"
 
 }
 
