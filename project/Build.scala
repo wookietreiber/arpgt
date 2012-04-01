b35a73e991d2431747fe31f187983a0de19ae682
Christian Krause <kizkizzbangbang@googlemail.com>
2012-04-01 13:51:27 +0200

    build update

--- project/Build.scala
+++ project/Build.scala
@@ -1,32 +1,18 @@
 import sbt._
 import Keys._
 
-import Dependencies._
-import BuildSettings._
-
-object BuildSettings {
-  val buildOrganization = "com.github.arpgt"
-  val buildVersion      = "0.1"
-  val buildScalaVersion = "2.9.1"
-
-  val buildSettings = Defaults.defaultSettings ++ Seq (
-    organization := buildOrganization,
-    version      := buildVersion,
-    scalaVersion := buildScalaVersion
-  )
-}
-
 object ArpgtBuild extends Build {
   lazy val root = Project ( "arpgt", file ("."),
-    settings = buildSettings ++ Seq (
-      libraryDependencies ++= Seq ( specs2 ),
-      initialCommands in Compile in console := """
+    settings = Defaults.defaultSettings ++ Seq (
+      organization := "com.github.arpgt",
+      version      := "0.1.0-SNAPSHOT",
+      scalaVersion := "2.9.1",
+      libraryDependencies ++= Seq (
+        "org.specs2" %% "specs2" % "1.8.2" % "test"
+      ),
+      initialCommands in Compile in console  += """
         import rpg._
       """
     )
   )
-}
-
-object Dependencies {
-  val specs2 = "org.specs2" %% "specs2" % "1.8.2" % "test"
 }
