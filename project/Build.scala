5679ade3b885576c7846d0fc2ac9f3029bcfa332
Christian Krause <kizkizzbangbang@googlemail.com>
2012-04-08 12:35:59 +0200

    build and version update

--- project/Build.scala
+++ project/Build.scala
@@ -4,11 +4,12 @@ import Keys._
 object ArpgtBuild extends Build {
   lazy val root = Project ( "arpgt", file ("."),
     settings = Defaults.defaultSettings ++ Seq (
-      organization := "com.github.arpgt",
-      version      := "0.1.0-SNAPSHOT",
-      scalaVersion := "2.9.1",
+      organization         := "com.github.arpgt",
+      version              := "0.1.0-SNAPSHOT",
+      scalaVersion         := "2.9.2-RC2",
+      crossScalaVersions   := Seq ( "2.9.1", "2.9.1-1", "2.9.2-RC2" ),
       libraryDependencies ++= Seq (
-        "org.specs2" %% "specs2" % "1.8.2" % "test"
+        "org.specs2" %% "specs2" % "1.9" % "test"
       ),
       initialCommands in Compile in console  += """
         import rpg._
