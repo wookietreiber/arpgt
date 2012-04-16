c70295f168f7fcce99b8eef5f3a8942713bed3ea
Christian Krause <kizkizzbangbang@googlemail.com>
2012-04-16 22:03:06 +0200

    scala version update

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ object ArpgtBuild extends Build {
     settings = Defaults.defaultSettings ++ Seq (
       organization         := "com.github.arpgt",
       version              := "0.1.0-SNAPSHOT",
-      scalaVersion         := "2.9.1-1",
-      crossScalaVersions   := Seq ( "2.9.1", "2.9.1-1", "2.9.2-RC2" ),
+      scalaVersion         := "2.9.2",
+      crossScalaVersions   := Seq ( "2.9.1", "2.9.1-1", "2.9.2" ),
       libraryDependencies ++= Seq (
         "org.specs2" %% "specs2" % "1.9" % "test"
       ),
