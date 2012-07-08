02f8a9aadf6c0917cd72059d6d719c0eb70d76a9
Christian Krause <kizkizzbangbang@googlemail.com>
2012-07-08 12:09:56 +0200

    build update

--- project/Build.scala
+++ project/Build.scala
@@ -9,9 +9,10 @@ object ArpgtBuild extends Build {
       scalaVersion         := "2.9.2",
       crossScalaVersions   := Seq ( "2.9.1", "2.9.1-1", "2.9.2" ),
       libraryDependencies ++= Seq (
-        "org.specs2" %% "specs2" % "1.9" % "test"
+        "org.specs2" %% "specs2" % "1.11" % "test"
       ),
-      initialCommands in Compile in console  += """
+      initialCommands in (Compile, consoleQuick) <<= initialCommands in Compile,
+      initialCommands in Compile in console += """
         import rpg._
       """
     )
